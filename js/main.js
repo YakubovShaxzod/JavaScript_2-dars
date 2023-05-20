80a822dc4e078560a76c2780295551e7acb467df
YakubovShaxzod <133785760+YakubovShaxzod@users.noreply.github.com>
2023-05-20 15:20:56 +0500

    JavaScript2-dars
    Bu yerda  JavaScript2-dars kodlari yuklandi

---
+++ js/main.js
@@ -0,0 +1,88 @@
+/*if (agar)
+else (u holda)
+else if (yoki bolmasa)
+number son, string matn*/
+/*if (45 == "45"){
+    document.write("ular teng!");
+
+}else{
+    document.write("Ular tengmas!");
+
+}
+if (150*3 == "150*15"){
+    document.write('=')
+
+}else{
+    document.write("<")
+}
+if ('2*2+8*8' == '100/10*2+14'){
+    document.write("96");
+
+}else{
+    document.write("34")
+
+}
+if (44==45){
+    document.write("1-solishtirish teng")
+
+}else if (46==46){
+    document.write("2-solishtirish teng")
+
+}else {
+    document.write("tengmas")
+
+}
+let a = "Mers";
+let b = "Bmw";
+if (a>b){
+    document.write("Bmw");
+
+} else if ("Bmw" < "Mers"){
+    document.write("Mers");
+}else{
+    document.write("hech qaysi bir bolmaydi");
+
+}
+
+if (99 == 100){
+    document.write(1- togri);
+
+}else if (120*2>120/2){
+    document.write(2-togri);
+
+}else {
+    document.write("xato");
+
+}
+if (a == 11 && b == 19 && c == 46 && d == 49){
+    document.write("Shartlarning hammasi bajarildi");
+
+}else{
+    document.write("Shartlar toliq  bajarilmaan")
+
+}*/
+
+let a = 30*2;
+let b = 40/4;
+let c = 77*6;
+let d = 66*5;
+/*if (a == 30*2 && b == 40/4 && c == 77*6 && d == 66*5){
+    document.write("Vse oni rani");
+}else {
+    document.write("odin iz usloviy ne raven!");
+
+}
+if ( a == 30*88 || b == 40/10 || c == 77*10 || d == 66+5){
+    document.write("Vse oni ravni");
+
+}else {
+    document.write("odin iz usloviy ne raven");
+
+}*/
+/*JS data types : undefined, boolean, object*/
+/*let soz;
+    document.write(typeof soz)*/
+let rost = a == 30*2 && b == 40/4 && c == 77*6 && d ==66-5? true:false;
+document.write(rost);
+let xato = a == 30*2 || b == 65*5 || c == 40-9 || d == 66*5? true:false;
+document.write(xato)
